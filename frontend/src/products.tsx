9402b241186a9c697627fb10c3d8b0d82d933c52
Rafa Audibert <rafael@posthog.com>
2026-05-27 19:48:56 -0300

    refactor: Move Exports away from sidebar
    Exports makes no sense there. Move it to the "data" entry in the sidebar, and also expose in Settings for good measure.

--- frontend/src/products.tsx
+++ frontend/src/products.tsx
@@ -2225,6 +2225,14 @@ export const getTreeItemsMetadata = (): FileSystemImport[] => [
         sceneKeys: ['IngestionWarnings'],
     },
     {
+        path: 'Exports',
+        category: 'Pipeline',
+        iconType: 'exports',
+        href: urls.exports(),
+        sceneKey: 'Exports',
+        sceneKeys: ['Exports'],
+    },
+    {
         path: 'Managed viewsets',
         category: 'Unreleased',
         iconType: 'managed_viewsets',
